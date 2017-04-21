7ca390212dc4fab15c2f1c220644a17b856d09e5
Prateek R. Patil <prateek.patil@gmail.com>
2017-04-21 09:58:01 -0700

    Changed some stuff about integer parsing.

--- printf/src/pfc_signed_int.c
+++ printf/src/pfc_signed_int.c
@@ -6,7 +6,7 @@
 /*   By: ppatil <ppatil@student.42.us.org>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/18 13:38:33 by ppatil            #+#    #+#             */
-/*   Updated: 2017/04/18 16:56:06 by ppatil           ###   ########.fr       */
+/*   Updated: 2017/04/19 15:02:22 by ppatil           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,27 +24,22 @@ static char *pfc_si_l(va_list ap, int p)
 	return (ft_itoa_generic(va_arg(ap, long), 10, p, "-0123456789"));
 }
 
-static char *pfc_si_h(va_list ap, int p)
+static char *pfc_si_lljzt(va_list ap, int p)
 {
-	return (ft_itoa_generic((short)va_arg(ap, int), 10, p, "-0123456789"));
+	return (ft_itoa_generic(va_arg(ap, long long), 10, p, "-0123456789"));
 }
 
-static char *pfc_si_hh(va_list ap, int p)
+char		*pfc_signed_int(t_pf_arg arg, va_list ap)
 {
-	return (ft_itoa_generic((char)va_arg(ap, int), 10, p, "-0123456789"));
-}
+	char	*rv;
 
-static char *pfc_si_j(va_list ap, int p)
-{
-	return (ft_itoa_generic(va_arg(ap, intmax_t), 10, p, "-0123456789"));
-}
-
-static char *pfc_si_z(va_list ap, int p)
-{
-	return (ft_itoa_generic(va_arg(ap, size_t), 10, p, "-0123456789"));
-}
-
-static char *pfc_si_t(va_list ap, int p)
-{
-	return (ft_itoa_generic(va_arg(ap, ptrdiff_t), 10, p, "-0123456789"));
+	if (!arg.prec.exists)
+		arg.prec.precision = 1;
+	if (arg.len.l == 1)
+		rv = pfc_si_l(ap, arg.prec.precision);
+	else if (arg.len.l == 2 || arg.len.j || arg.len.z || arg.len.t)
+		rv = pfc_si_lljzt(ap, arg.prec.precision);
+	else
+		rv = pfc_si_default(ap, arg.prec.precision);
+	return (rv);
 }
